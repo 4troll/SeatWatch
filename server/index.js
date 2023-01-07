a9d5d08ef488c2e73ad542ec27e8b4808edc7d33
Mustafa Abdulameer <mustafaaabdulameer@gmail.com>
2023-01-06 23:40:04 -0500

    .

--- server/index.js
+++ server/index.js
@@ -244,15 +244,21 @@ app.post('/api/getCourse', [middleware.jsonParser], async (req, res) => {
 	//res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
 });
 
-app.use("/static", express.static("client"));
-
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/public', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+	// Serve any static files
+  
+	app.use(express.static(path.join(__dirname, '../client/build', "registerServiceWorker.js")));
+	app.use(express.static(path.join(__dirname, '../client/build')));
+  
+	// Handle React routing, return all requests to React app
+	app.get('*', function(req, res) {
+	  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+	});
+  }
 
 for (let i = 0; i < 4; ++i) {
 	const curYear = `${year + Math.floor((startingSeason + i) / 3)}`
@@ -371,3 +377,4 @@ agenda.define("queueCourses", async () => {
 // 	{term: "2231", course: "COMPSCI 1DM3"}
 //   );
 // scrapingQueue.start();
+
